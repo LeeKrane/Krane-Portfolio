b9655706341cccceffa40d6a90a59db9ce147900
krane <christian.kranabetter@protonmail.com>
2024-08-17 13:50:41 +0200

    add and configure image nuxt module

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -7,7 +7,7 @@ export default defineNuxtConfig({
 		compatibilityVersion: 4
 	},
 
-    modules: ["@nuxtjs/robots", "@nuxtjs/sitemap"],
+    modules: ["@nuxtjs/robots", "@nuxtjs/sitemap", "@nuxt/image"],
 
 	robots: {
 		disallow: []
@@ -16,5 +16,18 @@ export default defineNuxtConfig({
 	site: {
 		url: "https://example.com",
 		name: "Example Website",
+	},
+
+	image: {
+		quality: 75,
+		formats: ["avif", "webp", "jpg", "png"],
+		screens: {
+			'xs': 320,
+			'sm': 640,
+			'md': 768,
+			'lg': 1024,
+			'xl': 1280,
+			'2xl': 1536
+		},
 	}
 })
